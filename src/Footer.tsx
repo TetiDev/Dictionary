5122da3f9959887299643ca6dc9e990de03cc470
Tatiana <tetianahil.dev@gmail.com>
2022-08-06 22:19:27 +0200

    +

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer: React.FC = () => (
         <footer className="footer mt-5">
             <img src="https://img.kidico.com.ua/shecode/girl.png" width="30" alt="img"/>
             <p>@ 2022 Coded by Tetiana Hil</p>
-            <a href="https://github.com/TetiDev/weatherWidget" target="_blank" rel="noreferrer">
+            <a href="https://github.com/TetiDev/Dictionary" target="_blank" rel="noreferrer">
                 Open-source code
             </a>
         </footer>
