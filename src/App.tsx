362317b21f89dded392c9e59b3e35f4fa915f920
Tetiana <tetianahil.dev@gmail.com>
2023-09-13 14:22:00 -0700

    Added .env and refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import React, { useEffect, useState } from 'react';
 import './style/App.css';
 import './style/tabs.css';
 import axios from 'axios';
+import { Translate } from '@google-cloud/translate/build/src/v2';
 import { Search } from './page/Search';
 import { Footer } from './page/Footer';
 import { Body } from './page/Body';
 import { AxiosPexelsType, AxiosResponseType } from './AxiosResponseType';
+import { Capitalize } from './capitalize';
 
 function App() {
   const [word, setWord] = useState('');
+  const [dataTranslate, setDataTranslate] = useState('');
   const [data, setData] = useState<AxiosResponseType>({
     word: '',
     meanings: [],
@@ -20,6 +23,15 @@ function App() {
   const [dataPexels, setDataPexels] = useState<AxiosPexelsType>({
     photos: [],
   });
+  const [langTarget,setLangTarget] = useState<string>('ru');
+
+  const googleApiKey = process.env.GOOGLE_TRANSLATE_APP_API_KEY;
+  const projectId = process.env.GOOGLE_TRANSLATE_PROJECT_ID;
+
+  const translate = new Translate({
+    projectId,
+    key: googleApiKey,
+  });
 
   useEffect(() => {
     (
@@ -29,10 +41,12 @@ function App() {
         try {
           const response = await axios.get(url);
           const responsePexels = await axios.get(urlPexels, { headers: { Authorization: '563492ad6f91700001000001e0301533c7f94b83ad764347fac773be' } });
-          console.log(response);
-          console.log(responsePexels);
           setData(response.data[0]);
           setDataPexels(responsePexels.data);
+
+          const [translations] = await translate.translate(word, langTarget);
+          setDataTranslate(Capitalize(translations));
+          console.log(translations);
         } catch (e) {
           console.log('This is an error!');
         }
@@ -49,7 +63,7 @@ function App() {
         <div className="App">
             <Search onChange={handlerWordChange}/>
             {visible
-              ? <Body word={word} data={data} dataPexels={dataPexels}/>
+              ? <Body word={word} data={data} dataPexels={dataPexels} dataTranslate={dataTranslate}/>
               : null
             }
             <Footer/>
