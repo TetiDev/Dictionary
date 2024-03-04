fb66a09f88f94e9d43cf478695494ada89be3c27
Tetiana <tetianahil.dev@gmail.com>
2024-03-04 14:43:31 -0800

    Changed the photo`s link

--- src/page/Footer.tsx
+++ src/page/Footer.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 export const Footer: React.FC = () => (
     <div>
         <footer className="footer mt-5">
-            <img src="https://img.kidico.com.ua/shecode/girl.png" width="30" alt="img"/>
-            <p>@ 2023 Coded by Tetiana Hil</p>
+            <img src="autor.png" width="30" alt="img"/>
+            <p>@ 2024 Coded by Tetiana Hil</p>
             {/* <a href="https://github.com/TetiDev/Dictionary" target="_blank" rel="noreferrer"> */}
             {/*    Open-source code */}
             {/* </a> */}
